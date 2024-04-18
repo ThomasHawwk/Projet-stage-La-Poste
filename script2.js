1285f37c57275c82fc2b9cf9dce188125a91668e
ThomasHawwk <165882223+ThomasHawwk@users.noreply.github.com>
2024-04-18 11:22:49 +0200

    explications js

--- script2.js
+++ script2.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const form = document.querySelector('form'); // Utilisation de querySelector pour sélectionner le formulaire
+    const form = document.querySelector('form'); // selection du formulaire
     const errorMessage = document.getElementById('error-message');
     const successMessage = document.getElementById('success-message');
 
     form.addEventListener("submit", function(event) {
-        event.preventDefault(); // Empêche l'envoi du formulaire par défaut
+        event.preventDefault(); // attente remplissage avant envoi
 
         try {
             const name = document.getElementById('name').value;
@@ -12,21 +12,22 @@ document.addEventListener('DOMContentLoaded', function() {
             const message = document.getElementById('msg').value;
 
             if (name === '' || email === '' || message === '') {
-                throw new Error('Veuillez remplir tous les champs.'); // Lance une erreur si un champ est vide
+                throw new Error('Veuillez remplir tous les champs.'); // erreur si champ vide
             }
 
             if (!email.includes('@')) {
                 throw new Error('Veuillez saisir une adresse e-mail valide.');
-            }
-            // Affiche un message de confirmation
-            successMessage.textContent = 'Le formulaire de "' + name + '" a bien été transmis.';
+            } // obligation @ dans le mail 
+
+
+            successMessage.textContent = 'Le formulaire de "' + name + '" a bien été transmis.'; // message ok si le formulaire est transmis
             successMessage.style.display = 'block';
-            errorMessage.style.display = 'none'; // Cache le message d'erreur s'il est affiché
-            form.reset(); // Réinitialise le formulaire après la soumission
+            errorMessage.style.display = 'none'; // cache le message d'erreur s'il est affiché
+            form.reset(); // reset du formulaire si transmis
             } catch (error) {
-            errorMessage.textContent = error.message; // Affiche le message d'erreur
-            errorMessage.style.display = 'block'; // Affiche le message d'erreur
-            successMessage.style.display = 'none'; // Cache le message de succès s'il est affiché
+            errorMessage.textContent = error.message; // affichage du message d'erreur
+            errorMessage.style.display = 'block'; // affiche le message d'erreur
+            successMessage.style.display = 'none'; // cache le message succès
         }
     });
 });
